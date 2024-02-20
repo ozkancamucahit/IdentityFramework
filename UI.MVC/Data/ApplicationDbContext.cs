1529a3f3b47f57403baed37336ae3a4ca9ada899
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-02-20 17:38:37 +0300

    Add project files.

---
+++ UI.MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UI.MVC.Data
+{
+    public sealed class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt)
+        {
+            
+        }
+
+
+    }
+}
---
+++ UI.MVC/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace UI.MVC.Models
+{
+    public sealed class AppUser : IdentityUser
+    {
+        [Required]
+        [Length(4, 20)]
+        public string NickName { get; set; } = String.Empty;
+    }
+}
